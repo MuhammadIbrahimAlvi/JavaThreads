0cb5e64b19dc4441d1ad5cb0a48d9444340cd242
Muhammad Ibrahim Alvi <ebrahimalvi2@gmail.com>
2023-05-15 00:47:54 +0500

    Added the file of FileReader

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,8 +1,27 @@
-public class FileReader implements Runnable{
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Logger;
 
+public class FileReader implements Runnable{
+    Logger logger = Logger.getAnonymousLogger();
 
     @Override
     public void run() {
+        logger.info("Started :: run() method of Thread with reference: "+ Thread.currentThread() + "and of name: "+ Thread.currentThread().getName());
+
+        MySqlAccess mySqlAccess = new MySqlAccess();
+        try {
+           ResultSet resultSet = mySqlAccess.readDatabase("SELECT * FROM EMPLOYEES");
+
+           while (resultSet.next()) {
+               int id = resultSet.getInt("id");
+           }
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
