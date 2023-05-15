bd331550099942e65d1f15fecf317af9010d85bc
Muhammad Ibrahim Alvi <ebrahimalvi2@gmail.com>
2023-05-16 01:03:12 +0500

    Wrote run() method of FileReader

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,5 +1,6 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class FileReader implements Runnable{
@@ -12,14 +13,21 @@ public class FileReader implements Runnable{
         MySqlAccess mySqlAccess = new MySqlAccess();
         try {
            ResultSet resultSet = mySqlAccess.readDatabase("SELECT * FROM EMPLOYEES");
-
+           List<Map<Long, Employee.Builder>> employeeList = new ArrayList<>();
            while (resultSet.next()) {
-               int id = resultSet.getInt("id");
+               Map<Long, Employee.Builder> builderMap = new HashMap<>();
+               long id = resultSet.getLong("id");
+               String name = resultSet.getString("name");
+               String department = resultSet.getString("department");
+               long salary = resultSet.getLong("salary");
+               Date hireDate = resultSet.getDate("hire_date");
+               long age = resultSet.getLong("age");
+               Employee.Builder employee = new Employee.Builder(name, age, department, salary, hireDate);
+               builderMap.put(id, employee);
+               employeeList.add(builderMap);
            }
 
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
 
